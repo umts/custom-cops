9ea203dc3264af9b657a54ac5170312f24f7a91b
David Faulkenberry <david.faulkenberry@gmail.com>
2016-01-14 10:11:34 -0500

    Init

---
+++ lib/custom-cops.rb
@@ -0,0 +1,7 @@
+require 'custom-cops/version'
+
+require 'custom-cops/be_for_singleton'
+require 'custom-cops/predicate_method_matcher'
+
+module CustomCops
+end
---
+++ lib/custom-cops/version.rb
@@ -0,0 +1,3 @@
+module CustomCops
+  VERSION = "0.1.0"
+end
