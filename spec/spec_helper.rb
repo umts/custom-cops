ffcd6e0516231038dcec4d501021411d909cca65
David Faulkenberry <david.faulkenberry@gmail.com>
2016-01-14 13:35:55 -0500

    More specific require statement

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ rubocop_path = File.join(File.dirname(__FILE__), '../vendor/rubocop')
 unless File.directory?(rubocop_path)
   fail "Can't run specs without a local RuboCop checkout. See README."
 end
-Dir["#{rubocop_path}/spec/support/**/*.rb"].each(&method(:require))
+require "#{rubocop_path}/spec/support/cop_helper"
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
