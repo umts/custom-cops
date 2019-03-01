c2303747957d6b334045ee959f88f001d93cb19e
Anbranin <keichelman@gmail.com>
2019-03-01 13:20:00 -0500

    rubocop moved support files to lib directory, so adding gem to vendor is no longer necessary

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,12 +6,7 @@ SimpleCov.start do
   refuse_coverage_drop
 end
 
-rubocop_path = File.join(File.dirname(__FILE__), '../vendor/rubocop')
-unless File.directory?(rubocop_path)
-  fail "Can't run specs without a local RuboCop checkout. See README."
-end
-require "#{rubocop_path}/spec/support/cop_helper"
-
+require 'rubocop/rspec/support'
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'umts-custom-cops'
